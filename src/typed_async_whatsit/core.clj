e2afd7096aa50e7338e1f7af89e190ac1ff0d2d8
Gary Fredericks <fredericksgary@gmail.com>
2014-12-02 10:05:32 -0600

    Just one go loop

--- src/typed_async_whatsit/core.clj
+++ src/typed_async_whatsit/core.clj
@@ -3,15 +3,12 @@
             [clojure.core.typed :as t]
             [clojure.core.typed.async :as t.a]))
 
-(t/ann connect [-> '{:in (t.a/Chan String)
-                     :out (t.a/Chan String)}])
+(t/ann connect [-> nil])
 (defn connect
   []
   (let [ch (t.a/chan :- String 100)]
     (t.a/go-loop []
-      (if-let [s (a/<! ch)]
-        (recur)))
-    (t.a/go-loop []
       (let [s "hey hey"]
         (a/>! ch s)
-        (recur)))))
+        (recur)))
+    nil))
