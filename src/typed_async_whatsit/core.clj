97c53ae7aa82156e846d723a729de361e96af33a
Gary Fredericks <fredericksgary@gmail.com>
2014-12-02 10:03:49 -0600

    Minimize further

--- src/typed_async_whatsit/core.clj
+++ src/typed_async_whatsit/core.clj
@@ -3,19 +3,15 @@
             [clojure.core.typed :as t]
             [clojure.core.typed.async :as t.a]))
 
-(t/ann connect [String Long -> '{:in (t.a/Chan String)
-                                 :out (t.a/Chan String)}])
+(t/ann connect [-> '{:in (t.a/Chan String)
+                     :out (t.a/Chan String)}])
 (defn connect
-  [host port]
-  (let [ch-in (t.a/chan :- String 100)
-        ch-out (t.a/chan :- String 100)]
+  []
+  (let [ch (t.a/chan :- String 100)]
     (t.a/go-loop []
-      (if-let [s (a/<! ch-out)]
-        (do
-          (println s)
-          (recur))))
+      (if-let [s (a/<! ch)]
+        (recur)))
     (t.a/go-loop []
       (let [s "hey hey"]
-        (a/>! ch-in s)
-        (recur)))
-    {:in ch-in, :out ch-out}))
+        (a/>! ch s)
+        (recur)))))
