1eaf98441d4ccaabb99e31001afda08f19af0a11
Gary Fredericks <fredericksgary@gmail.com>
2014-12-02 10:01:42 -0600

    no more io/socket noise

--- src/typed_async_whatsit/core.clj
+++ src/typed_async_whatsit/core.clj
@@ -1,36 +1,21 @@
 (ns typed-async-whatsit.core
   (:require [clojure.core.async :as a]
             [clojure.core.typed :as t]
-            [clojure.core.typed.async :as t.a]
-            [clojure.java.io :as io])
-  (:import [java.net Socket]))
-
-(t/ann ^:no-check clojure.java.io/reader
-       [java.net.Socket -> java.io.Reader])
-(t/ann ^:no-check clojure.java.io/writer
-       [java.net.Socket -> java.io.Writer])
+            [clojure.core.typed.async :as t.a]))
 
 (t/ann connect [String Long -> '{:in (t.a/Chan String)
                                  :out (t.a/Chan String)}])
 (defn connect
   [host port]
   (let [ch-in (t.a/chan :- String 100)
-        ch-out (t.a/chan :- String 100)
-        s (Socket. ^String host (int port))
-        r (io/reader s)
-        w (io/writer s)]
+        ch-out (t.a/chan :- String 100)]
     (t.a/go-loop []
       (if-let [s (a/<! ch-out)]
         (do
-          (binding [*out* w]
-            (println s))
-          (recur))
-        (do
-          (.close r)
-          (.close w)
-          (.close s))))
+          (println s)
+          (recur))))
     (t.a/go-loop []
-      (let [s (binding [*in* r] (read-line))]
+      (let [s "hey hey"]
         (a/>! ch-in s)
         (recur)))
     {:in ch-in, :out ch-out}))
